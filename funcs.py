bf302ac1170ea72f703a246917600e7a1d54a1a3
Sergey Zamuraev <zamuraev210854@gmail.com>
2024-02-07 17:38:01 +0300

    add razn func

--- funcs.py
+++ funcs.py
@@ -3,3 +3,10 @@ def summma2x():
     b = input("Введите второе число")
     c = a + b
     return c
+
+def vichit2x():
+    a = input("Введите первое число")
+    b = input("Введите второе число")
+    c = a - b
+    return c
+
